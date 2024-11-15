7d98b86468ba0ec133d3e2499aec193754358b8d
Saswata Biswas <saswata.biswas20@gmail.com>
2024-11-15 17:17:40 +0530

    Added auth0

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const { auth } = require('./middleware');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = "secret";
-const USERS = [];
-let USER_ID_COUNTER = 1;
+const { auth } = require('express-oauth2-jwt-bearer');
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const cors = require('cors');
+const dotenv = require('dotenv');
 app.use(cors());
 app.use(jsonParser);
+dotenv.config();
 
-const Problems = require('./problems');
-const PROBLEMS = Problems;
+const domain = process.env.DOMAIN;
+
+const jwtCheck = auth({
+    audience: `https://es-code-client.vercel.app/`,
+    issuerBaseURL: 'https://dev-skne2ots1cwlwxnk.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+  });
+
+const PROBLEMS = require('./problems');
+
 const SUBMISSIONS = [];
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
+  
 app.get('/problems', (req, res) => {
     return res.json(PROBLEMS);
+});
+
+app.get('/secure', jwtCheck, (req, res) => {
+    res.send('Secure Route');
 });
 
 app.get('/problem/:problemId', (req, res) => {
@@ -35,98 +40,14 @@ app.get('/problem/:problemId', (req, res) => {
     return res.json({ problem });
 });
 
-app.post('/signup', (req, res) => {
-    const { username, password } = req.body;
-    const user = USERS.find(x => x.username === username);
 
-    if (user) {
-        return res.json({ msg: "User already exists" });
-    }
-
-    USERS.push({ id: USER_ID_COUNTER++, username, password });
-    return res.json({ msg: "User signed up!" });
-});
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = USERS.find(x => x.username === username);
-
-    if (!user) {
-        return res.status(404).json({ msg: "User not found!" });
-    }
-
-    if (user.password !== password) {
-        return res.status(403).json({ msg: "Incorrect password!" });
-    }
-
-    const token = jwt.sign({ id: user.id }, JWT_SECRET);
-    return res.json({ token });
-});
-
-app.get('/me', auth, (req, res) => {
-    const user = USERS.find(x => x.id === req.userId);
-    if (!user) {
-        return res.status(403).json({ msg: "User doesn't exist" });
-    }
-    return res.json({ user });
-});
-
-app.post('/submission', auth, async (req, res) => {
-    
-    const code = req.body.submission;
-    const problemId = req.body.problemId;
-
-    const problem = PROBLEMS.find(problem => problem.problemId === problemId);
-    const inputs = problem.stdin;
-
-    console.log(inputs)
-
-    let testCasesPassed = 0;
-    let failedCases = [];
-
-    for(let i=0; i < inputs.length; i++){
-
-        element = inputs[i];
-
-        const submit = await fetch('https://j2gra3n3eb.execute-api.us-west-1.amazonaws.com/prod', {
-            method: "POST",
-            body: JSON.stringify({
-                code: code,
-                inputs: element
-            })
-        })
-    
-        console.log('request submitted');
-
-        const output = await submit.json();
-        const expected_output = problem.stdout[i];
-        console.log(output);
-
-        if(output.output === expected_output){
-            testCasesPassed++;
-        }else{
-            failedCases.push(i);
-            console.log(`Test case ${i+1} failed`);
-        }
-    
-    };
-
-    console.log(testCasesPassed);
-
-    SUBMISSIONS.push(JSON.stringify({
-        problemId,
-        code: code,
-        testCasesPassed,
-        time: new Date()
-    }))
-    
-    return res.json({failedCases: failedCases});
-
-});
-
-app.post('/submission/cpp', auth, async (req, res) => {
+app.post('/submission/cpp', jwtCheck, async (req, res) => {
     const source_code = req.body.submission;
     const problemId = req.body.problemId;
+    const username = req.body.username;
+    const language = req.body.language;
+
+    let lang_id = language === 'cpp' ? 53 : 62;
 
     const problem = PROBLEMS.find(problem => problem.problemId === problemId);
     if(!problem){
@@ -150,7 +71,7 @@ app.post('/submission/cpp', auth, async (req, res) => {
             body: JSON.stringify({
                 source_code: source_code,
                 stdin: stdin,
-                language_id: 53
+                language_id: lang_id
             })
         })
 
@@ -189,14 +110,15 @@ app.post('/submission/cpp', auth, async (req, res) => {
         problemId,
         code: source_code,
         testCasesPassed,
-        time: new Date()
+        time: new Date(),
+        username: username
     }))
     
     return res.json({failedCases: failedCases});
 
 })
 
-app.get('/submissions/:problemId', auth, (req, res) => {
+app.get('/submissions/:problemId', jwtCheck, (req, res) => {
     const { problemId } = req.params;
     const submissions = SUBMISSIONS.filter(x => x.problemId === problemId && x.userId === req.userId);
     return res.json({ submissions });
